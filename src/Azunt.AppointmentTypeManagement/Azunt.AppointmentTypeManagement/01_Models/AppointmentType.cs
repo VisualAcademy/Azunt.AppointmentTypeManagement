0ba5af7c89cfda2d9639210c38922873861cceca
Park YongJune <redplus@redplus.net>
2025-07-14 22:36:48 +0900

    Add AppointmentType model class

---
+++ src/Azunt.AppointmentTypeManagement/Azunt.AppointmentTypeManagement/01_Models/AppointmentType.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Azunt.AppointmentTypeManagement
+{
+    public class AppointmentType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? AppointmentTypeName { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+    }
+}
