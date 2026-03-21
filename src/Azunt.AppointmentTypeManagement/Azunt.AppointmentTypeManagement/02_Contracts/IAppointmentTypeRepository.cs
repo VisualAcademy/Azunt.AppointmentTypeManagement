6255a09cd4d5838b8815505c570e136fbd6c672d
Park YongJune <redplus@redplus.net>
2026-03-22 06:57:30 +0900

    Add IAppointmentTypeRepository inferface file

---
+++ src/Azunt.AppointmentTypeManagement/Azunt.AppointmentTypeManagement/02_Contracts/IAppointmentTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Azunt.AppointmentTypeManagement
+{
+    public interface IAppointmentTypeRepository
+    {
+        Task AddAsync(AppointmentType appointmentType, string connectionString);
+        Task<List<AppointmentType>> GetAllAsync(string connectionString);
+        Task<AppointmentType> GetByIdAsync(long id, string connectionString);
+        Task UpdateAsync(AppointmentType appointmentType, string connectionString);
+        Task DeleteAsync(long id, string connectionString);
+    }
+}
